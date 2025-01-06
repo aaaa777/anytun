4ab670640d61fb767ea5237205845bf8f34128c3
aaaa777 <aaaa777@users.noreply.github.com>
2025-01-07 03:20:53 +0900

    fix: function

--- client/mac/src/anytund.sh
+++ client/mac/src/anytund.sh
@@ -21,23 +21,31 @@ kill-anytund() {
 # }
 
 get-if-data() {
-    latest_if_data=$(ifconfig -u)
+    ifconfig -u
 }
 
 get-host-ip() {
-    latest_if_ip=$(ifconfig en0 | grep 'inet' | awk '{print $2}')
+    ifconfig en0 | grep 'inet' | awk '{print $2}'
 }
 
 get-host-dhcp-dns() {
-    latest_dns_ip=$(ipconfig getpacket en0 | grep 'domain_name_server' | awk -F'[{}]' '{print $2}' | awk -F', ' '{print $1}')
+    ipconfig getpacket en0 | grep 'domain_name_server' | awk -F'[{}]' '{print $2}' | awk -F', ' '{print $1}'
 }
 
 get-host-default-dns() {
-    latest_dns_ip=$(scutil --dns | grep 'nameserver\[[0-9]*\]' | awk '{print $3}' | head -n 1)
+    scutil --dns | grep 'nameserver\[[0-9]*\]' | awk '{print $3}' | head -n 1
 }
 
-get-anytun-dns-override() {
-    return $(cat $CONFIG_DIR/client-config.json | jq -r '.dns.override')
+get-anytun-dns-override-type() {
+    cat $CONFIG_DIR/client-config.json | jq -r '.dns.override_type'
+}
+
+is-cisco-umbrella() {
+    if [[ "$(dig @$latest_dns_ip internetbadguys.com +short +time=1 2> /dev/null)" == "146.112.61.108" ]]; then
+        echo 1
+    else
+        echo 0
+    fi
 }
 
 main() {
@@ -47,16 +55,19 @@ main() {
     dns_ip=""
     # if_ip=""
     if_data=""
+    is_cisco_umbrella=0
 
     while true; do
         # TODO: client-config.jsonから判定条件を分岐できるようにする
-        get-if-data
-        # get-host-ip
+        latest_if_data=$(get-if-data)
+        # latest_if_ip=$(get-host-ip)
+        # $(get-anytun-dns-override-type)
 
         # if構成が変わったタイミングでチェックを行う
+        is_cisco_umbrella=$(is-cisco-umbrella)
         if [[ "$latest_if_data" != "$if_data" ]]; then
             echo "interface change detected"
-            get-host-dhcp-dns
+            latest_dns_ip=$(get-host-dhcp-dns)
             echo "new dns ip address: $latest_dns_ip"
             # DNSがcisco umbrellaかどうかを確認する
             latest_site_ip=$(dig @$latest_dns_ip internetbadguys.com +short +time=1 2> /dev/null)
