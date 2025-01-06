05131c2429c07d84b4c90abd3cc50db8b4534b4b
aaaa777 <aaaa777@users.noreply.github.com>
2025-01-07 04:20:23 +0900

    fix: some

--- client/mac/src/anytund.sh
+++ client/mac/src/anytund.sh
@@ -8,6 +8,7 @@
 # cd $SCRIPT_DIR
 
 CONFIG_DIR=MACRO_ANYTUN_CONFIG_DIR # anytun: set_config_dir
+ANYTUN_EXECUTABLE=MACRO_ANYTUN_EXECUTABLE # anytun: set_executable
 
 kill-anytund() {
     pkill -f anytund
@@ -36,16 +37,32 @@ get-host-default-dns() {
     scutil --dns | grep 'nameserver\[[0-9]*\]' | awk '{print $3}' | head -n 1
 }
 
+# not implemented yet
 get-anytun-dns-override-type() {
-    cat $CONFIG_DIR/client-config.json | jq -r '.dns.override_type'
+    type="$(cat $CONFIG_DIR/client-config.json | jq -r '.dns.override_type')"
+    if [[ "$type" == "dhcp" ]]; then
+        echo "dhcp"
+    elif [[ "$type" == "static" ]]; then
+        echo "static"
+    else
+        echo "default"
+    fi
 }
 
 is-cisco-umbrella() {
-    if [[ "$(dig @$latest_dns_ip internetbadguys.com +short +time=1 2> /dev/null)" == "146.112.61.108" ]]; then
+    if [[ "$(dig @$1 internetbadguys.com +short +time=1 2> /dev/null)" == "146.112.61.108" ]]; then
         echo 1
     else
         echo 0
     fi
+}
+
+start-anytun() {
+    eval "$ANYTUN_EXECUTABLE start"
+}
+
+stop-anytun() {
+    eval "$ANYTUN_EXECUTABLE stop"
 }
 
 main() {
@@ -53,45 +70,57 @@ main() {
         echo "you cant run this script directly."
         exit 1
     fi
-    
+
+    if [[ $2 == "--dry" ]]; then
+        echo "dry run"
+        DRY_RUN=1
+    fi
+
     kill-anytund
     INTERFACE="en0"
-    site_ip=""
-    dns_ip=""
+    prev_site_ip=""
+    prev_dns_ip=""
     # if_ip=""
-    if_data=""
+    prev_if_data=""
     is_cisco_umbrella=0
 
     while true; do
         # TODO: client-config.jsonから判定条件を分岐できるようにする
-        latest_if_data=$(get-if-data)
+        if_data=$(get-if-data)
         # latest_if_ip=$(get-host-ip)
         # $(get-anytun-dns-override-type)
 
         # if構成が変わったタイミングでチェックを行う
-        is_cisco_umbrella=$(is-cisco-umbrella)
-        if [[ "$latest_if_data" != "$if_data" ]]; then
+        if [[ "$prev_if_data" != "$if_data" ]]; then
             echo "interface change detected"
-            latest_dns_ip=$(get-host-dhcp-dns)
-            echo "new dns ip address: $latest_dns_ip"
+            # delay
+            sleep 1
+            dns_ip=$(get-host-default-dns)
+            echo "new dns ip address: $dns_ip"
             # DNSがcisco umbrellaかどうかを確認する
-            latest_site_ip=$(dig @$latest_dns_ip internetbadguys.com +short +time=1 2> /dev/null)
-            if [[ "$latest_site_ip" != "$site_ip" ]]; then
-                # cisco umbrellaの場合このサイトはデフォルトで固定のIPを返す
-                if [[ "146.112.61.108" == "$latest_site_ip" ]]; then
+            # site_ip=$(dig @$dns_ip internetbadguys.com +short +time=1 2> /dev/null)
+            is_cisco_umbrella=$(is-cisco-umbrella $dns_ip)
+            echo "is cisco umbrella: $is_cisco_umbrella"
+            echo "DRY_RUN: $DRY_RUN"
+
+            if [[ $DRY_RUN != "1" ]]; then
+                if [[ $is_cisco_umbrella == 1 ]]; then
                     # anytunを起動する
                     echo "cisco umbrella detected"
-                    eval "anytun start"
+                    start-anytun
                 else
                     # anytunを停止する
                     echo "network is clean"
-                    eval "anytun stop"
+                    stop-anytun
                 fi
+            else
+                echo "skipping anytun start/stop"
             fi
-            site_ip="$latest_site_ip"
-            # if_ip="$latest_if_ip"
-            if_data="$latest_if_data"
+            prev_site_ip="$site_ip"
+            # prev_if_ip="$if_ip"
+            prev_if_data="$if_data"
         fi
+        # interval
         sleep 1
     done
 }
